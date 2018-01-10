380b0c049320faa9f1ad82a96e575101d42adfde
tangp <tangp@dtds.com.cn>
2018-01-10 13:51:46 +0800

    渲染数据列表

--- src/models/my/App.js
+++ src/models/my/App.js
@@ -3,8 +3,21 @@ import './app.less';
 
 class App extends Component {
   render() {
+    const users =  [
+      { username: 'Jerry', age: 21, gender: 'male', id: 1 },
+      { username: 'Tomy', age: 22, gender: 'male', id: 2 },
+      { username: 'Lily', age: 19, gender: 'female', id: 3 },
+      { username: 'Lucy', age: 20, gender: 'female', id: 4 }
+    ]
+    const listItems = users.map((value) =>
+      <li key={value.id}>
+        <div>姓名：{value.username}</div>
+        <div>年龄：{value.age}</div>
+        <div>性别：{value.gender}</div>
+      </li>
+    );
     return (
-      <div className="title">个人中心</div>
+      <div><ul>{listItems}</ul></div>
     );
   }
 }
