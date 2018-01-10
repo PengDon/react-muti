9799a306ec6d5c77fc910f4eab8e5c49cde29750
tangp <tangp@dtds.com.cn>
2018-01-10 13:55:32 +0800

    静态数据渲染列表 方式1

--- src/models/my/App.js
+++ src/models/my/App.js
@@ -3,17 +3,17 @@ import './app.less';
 
 class App extends Component {
   render() {
-    const users =  [
+    const userList =  [
       { username: 'Jerry', age: 21, gender: 'male', id: 1 },
       { username: 'Tomy', age: 22, gender: 'male', id: 2 },
       { username: 'Lily', age: 19, gender: 'female', id: 3 },
       { username: 'Lucy', age: 20, gender: 'female', id: 4 }
     ]
-    const listItems = users.map((value) =>
-      <li key={value.id}>
-        <div>姓名：{value.username}</div>
-        <div>年龄：{value.age}</div>
-        <div>性别：{value.gender}</div>
+    const listItems = userList.map((user) =>
+      <li key={user.id}>
+        <div>姓名：{user.username}</div>
+        <div>年龄：{user.age}</div>
+        <div>性别：{user.gender}</div>
       </li>
     );
     return (
